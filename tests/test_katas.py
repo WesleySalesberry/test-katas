08a60a14cf77cf68a7edcfb02fa92d6ddaa69ea4
Wes <wesleySalesberry@hotmail.com>
2020-09-13 21:31:21 -0400

    first Attempt

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -1,22 +1,33 @@
 import unittest
 import katas
+import random
+
+x = random.randrange(1, 50, 3)
+y = random.randrange(1, 50, 3)
+n = random.randrange(1, 50, 3)
+
+add_answer = x + y
+multiply_answer = x * y
+power_answer = x**n
 
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        self.assertEqual(katas.add(x, y), add_answer)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        self.assertEqual(katas.multiply(x, y), multiply_answer)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        self.assertEqual(power(x, n), power_answer)
 
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        fact_answer = katas.factorial(5)
+        self.fail(fact_answer, 120)
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
+        fib_answer = katas.fibonacci(50)
+        self.fail(fib_answer, 12586269025)
 
 
 if __name__ == '__main__':
